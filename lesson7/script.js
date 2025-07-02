cad50ff967cb3ac699d408f51bbbfa4a69cd1421
Luka Rekhviashvili <rekhviashvili.luka125@gmail.com>
2025-07-02 19:52:46 +0400

    add lesson7

---
+++ lesson7/script.js
@@ -0,0 +1,38 @@
+// Syntax, pass nothing, returns nothing
+// function printHello() {
+//   console.log("Gamarjoba");
+// }
+
+// printHello();
+// printHello();
+// printHello();
+
+// pass information
+// function greet(name) {
+//   let res = "Hello " + name;
+//   console.log(res);
+// }
+
+// greet("Luka");
+// greet("Saba");
+// greet("Ana");
+// greet("Rezo");
+// greet("Zura");
+// greet("Irakli");
+
+// return information based on given information
+// function multiplyByTwo(num) {
+//   let res = num + num;
+//   return res;
+// }
+
+// let answer = multiplyByTwo(5);
+// console.log(answer);
+
+// more than one argument
+function addTwoNumbers(a, b) {
+  let res = a + b;
+  return res;
+}
+
+console.log(addTwoNumbers(1, 2));
